44116573573a3168d679a89ff29f1e72e4c99755
Yves Chevallier <yves.chevallier@heig-vd.ch>
2019-11-01 10:47:55 +0100

    Fix algorithms

--- algorithms/5.c
+++ algorithms/5.c
@@ -1,6 +1,6 @@
 /**
  * Afficher un cercle tel que:
- * 
+ *
  * $ ./a.out 4
  *         xx
  *     x        x
@@ -25,15 +25,15 @@
 int ratio = 2;
 char dot = 'x';
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     if (argc < 2) return 1;
 
     int radius = atoi(argv[1]);
 
-    for (int i = radius; i >= -radius; i--) 
+    for (int i = radius; i >= -radius; i--)
     {
-        int x = radius * cos(asin((float)i / radius));    
+        int x = radius * cos(asin((float)i / radius));
         for (int j = 0; j < ratio * (radius - x); j++) putchar(' ');
         putchar(dot);
         for (int j = 0; j < ratio * 2 * x; j++) putchar(' ');
