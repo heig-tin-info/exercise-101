e864955ce2bf8b0ef5e162e981228bdbfd8fad6a
Yves Chevallier <yves.chevallier@heig-vd.ch>
2020-02-14 15:43:35 +0100

    Modify 1.c

--- arguments/1.c
+++ arguments/1.c
@@ -2,3 +2,7 @@
  * Écrire un programme qui affiche le nombre d'arguments passés à un programme,
  * ignorant le nom du programme.
  */
+
+#include <stdio.h>
+
+   int main(int argc, char* argv[]) { printf("%d\n", argc -1 );}
