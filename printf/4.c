e9c7b7f1dcc9e18e7586a43b22cee750fe23169e
Yves Chevallier <yves-chevallier@heig-vd.ch>
2020-10-26 23:32:47 +0100

    still work in progress

--- printf/4.c
+++ printf/4.c
@@ -5,13 +5,13 @@
  * - Nombres de chiffres après la virgule 2
  * - Nombre de 0 devant le nombre 5
  *
- * Exemple: 00042.12
+ * Exemple: 0000042.12
  *
  */
 #include <stdio.h>
 
 int main(void) {
     double foo = 42.125;
-    printf("%07.2f\n", foo);
+    printf("%010.2f\n", foo);
     return 0;
 }
